d770440bebdbbf6220f3b02be8b51d26bb36fc89
Nizam <nizam.oezdemir@gmx.ch>
2021-10-27 16:41:21 +0200

    Push 4

--- ControllerTest2/Assets/Scripts/Player.cs
+++ ControllerTest2/Assets/Scripts/Player.cs
@@ -7,38 +7,21 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    //input fields
     private PlayerOneInput playerOneInput;
     private InputAction move;
-    private InputAction curve;
-
-    //movement fields
     private Rigidbody rb;
-    [SerializeField]
-    private float movementForce = 20f;
-    public float rotationSpeed = 200f;
-    [SerializeField]
-    private float maxSpeed = 100;
-    private float currentSpeed = 0;
-    private Vector3 forceDirection = Vector3.zero;
-    //private Vector3 curveDirection = Vector3.zero;
-
-    
-
-    [SerializeField]
-    private Camera playerCamera;
-    private Animator animator;
+    public float rotationSpeed = 20f;
+    public float maxSpeed = 100;
+    public float currentSpeed = 0;
 
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody>();
         playerOneInput = new PlayerOneInput();
-        animator = this.GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
-        //look = PlayerOneInput.PlayerOne.Look;
         move = playerOneInput.PlayerOne.Move;
         playerOneInput.PlayerOne.Enable();
     }
@@ -48,78 +31,26 @@ public class Player : MonoBehaviour
         playerOneInput.PlayerOne.Disable();
     }
 
-    private void FixedUpdate()
-    {
-        //Links/Rechts
-        //forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(playerCamera) * movementForce;
-
-        
-        if (currentSpeed < maxSpeed)
-        {
-            rb.AddRelativeForce(Vector3.forward * 50);
-        }
-        
-
-        //Beschleunigen
-        //forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(playerCamera) * movementForce;
-
-        //rb.AddForce(forceDirection, ForceMode.Impulse);
-        //rb.AddTorque(forceDirection, ForceMode.Impulse);
-        
-        //forceDirection = Vector3.zero;
-       // curveDirection = Vector3.zero;
-       /*
-        if (rb.velocity.y < 0f)
-        {
-            rb.velocity -= Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
-        }
-
-        Vector3 horizontalVelocity = rb.velocity;
-        horizontalVelocity.y = 0;
-        if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
-        {
-            rb.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * rb.velocity.y;
-        }
-       */
-    }
-
     private void Update()
     {
-        //forceDirection += move.ReadValue<Vector2>().y * GetCameraRight(playerCamera) * movementForce;
 
-        if (Input.GetAxis("Horizontal") > 0.2f)
+        rb.AddForce(Vector3.forward * 20f);
+
+
+        if (move.ReadValue<Vector2>().x > 0.1f)
         {
-            //transform.localRotation = Quaternion.Euler(forceDirection);
-            //transform.localRotation = Quaternion.Euler(new Vector3(0, forceDirection, 0));
-            rb.AddTorque(Vector3.up * rotationSpeed);
+            rb.AddTorque(Vector3.up * 10f, ForceMode.Impulse);
+            rb.AddForce(Vector3.right * 10f, ForceMode.Impulse);
+       
         } 
-        else if (Input.GetAxis("Horizontal") < -0.2f)
+        else if (move.ReadValue<Vector2>().x < -0.1f)
         {
-            //transform.localRotation = Quaternion.Euler(new Vector3(0, -forceDirection, 0));
-            //transform.localRotation = Quaternion.Euler(forceDirection);
-            rb.AddTorque(-Vector3.up *rotationSpeed);
+            rb.AddTorque(-Vector3.up * 10f, ForceMode.Impulse);
+            rb.AddForce(Vector3.left * 10f, ForceMode.Impulse);
         } 
         else
         {
-            //transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
-        }
-        //rb.AddForce(forceDirection, ForceMode.Impulse);
-        //forceDirection = Vector3.zero;
+
+        }        
     }
-
-
-    private Vector3 GetCameraForward(Camera playerCamera)
-    {
-        Vector3 forward = playerCamera.transform.forward;
-        forward.y = 0;
-        return forward.normalized;
-    }
-
-    private Vector3 GetCameraRight(Camera playerCamera)
-    {
-        Vector3 right = playerCamera.transform.right;
-        right.y = 0;
-        return right.normalized;
-    }
-
 }
--- ControllerTest2/Assets/Scripts/PlayerOneInput.cs
+++ ControllerTest2/Assets/Scripts/PlayerOneInput.cs
@@ -22,15 +22,7 @@ public class @PlayerOneInput : IInputActionCollection, IDisposable
                     ""name"": ""Move"",
                     ""type"": ""Value"",
                     ""id"": ""7e1d9210-ac00-4e9d-8503-261b482f6bbf"",
-                    ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
-                    ""interactions"": """"
-                },
-                {
-                    ""name"": ""Accelerate"",
-                    ""type"": ""Value"",
-                    ""id"": ""b70af3bf-f2f5-4ba4-930c-7abecff47e95"",
-                    ""expectedControlType"": ""Vector2"",
+                    ""expectedControlType"": ""Vector3"",
                     ""processors"": """",
                     ""interactions"": """"
                 }
@@ -55,7 +47,6 @@ public class @PlayerOneInput : IInputActionCollection, IDisposable
         // PlayerOne
         m_PlayerOne = asset.FindActionMap("PlayerOne", throwIfNotFound: true);
         m_PlayerOne_Move = m_PlayerOne.FindAction("Move", throwIfNotFound: true);
-        m_PlayerOne_Accelerate = m_PlayerOne.FindAction("Accelerate", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -106,13 +97,11 @@ public class @PlayerOneInput : IInputActionCollection, IDisposable
     private readonly InputActionMap m_PlayerOne;
     private IPlayerOneActions m_PlayerOneActionsCallbackInterface;
     private readonly InputAction m_PlayerOne_Move;
-    private readonly InputAction m_PlayerOne_Accelerate;
     public struct PlayerOneActions
     {
         private @PlayerOneInput m_Wrapper;
         public PlayerOneActions(@PlayerOneInput wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_PlayerOne_Move;
-        public InputAction @Accelerate => m_Wrapper.m_PlayerOne_Accelerate;
         public InputActionMap Get() { return m_Wrapper.m_PlayerOne; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -125,9 +114,6 @@ public class @PlayerOneInput : IInputActionCollection, IDisposable
                 @Move.started -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnMove;
                 @Move.performed -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnMove;
                 @Move.canceled -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnMove;
-                @Accelerate.started -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnAccelerate;
-                @Accelerate.performed -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnAccelerate;
-                @Accelerate.canceled -= m_Wrapper.m_PlayerOneActionsCallbackInterface.OnAccelerate;
             }
             m_Wrapper.m_PlayerOneActionsCallbackInterface = instance;
             if (instance != null)
@@ -135,9 +121,6 @@ public class @PlayerOneInput : IInputActionCollection, IDisposable
                 @Move.started += instance.OnMove;
                 @Move.performed += instance.OnMove;
                 @Move.canceled += instance.OnMove;
-                @Accelerate.started += instance.OnAccelerate;
-                @Accelerate.performed += instance.OnAccelerate;
-                @Accelerate.canceled += instance.OnAccelerate;
             }
         }
     }
@@ -145,6 +128,5 @@ public class @PlayerOneInput : IInputActionCollection, IDisposable
     public interface IPlayerOneActions
     {
         void OnMove(InputAction.CallbackContext context);
-        void OnAccelerate(InputAction.CallbackContext context);
     }
 }
