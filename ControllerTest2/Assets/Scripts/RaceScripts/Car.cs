45995dab73a20e119508a7c34ccd8db27d7934fc
Michael Stucki <mipictrost@gmail.com>
2021-11-03 14:47:19 +0100

    stumis code added

---
+++ ControllerTest2/Assets/Scripts/RaceScripts/Car.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Car : MonoBehaviour
+{
+
+    private InputAction upDown;
+    private TrackBuilderControl trackBuilderControl;
+    private int physicUpdateCount;
+
+    private bool isLeftTurnAllowed = true;
+    private bool isRightTurnAllowed = true;
+
+    [SerializeField]
+    private trackCreator track;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        trackBuilderControl = new TrackBuilderControl();
+    }
+    private void OnEnable()
+    {
+        upDown = trackBuilderControl.TrackBuilder.UpDown;
+        trackBuilderControl.TrackBuilder.Enable();
+    }
+
+    private void OnDisable()
+    {
+        trackBuilderControl.TrackBuilder.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (physicUpdateCount % 2 == 0)
+        {
+            if (physicUpdateCount > 20) //Head Start for Track
+            {
+                this.DriveCar();
+            }
+        }
+           
+        physicUpdateCount++;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("the car hit: " + other.name);
+        switch (other.name)
+        {
+            case "leftBorder":
+                isLeftTurnAllowed = false;
+                break;
+            case "rightBorder":
+                isRightTurnAllowed = false;
+                break;
+        }
+        Debug.Log("hit at: " + other.gameObject.transform.forward);
+        transform.rotation = Quaternion.LookRotation(other.gameObject.transform.forward, transform.up);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isLeftTurnAllowed = isRightTurnAllowed = true;
+    }
+    private void DriveCar()
+    {
+        Vector3[] currentCarUp = track.getCurrentCarUp(transform.position);
+        float steering = upDown.ReadValue<Vector2>().x * 0.1f;
+        if(!isLeftTurnAllowed && steering < 0 || !isRightTurnAllowed && steering > 0)
+        {
+            steering = 0;
+        }
+        Vector3 steeringInput = new Vector3(steering, 0, 0);
+        Vector3 newForward = transform.rotation * steeringInput + transform.forward;
+
+        Vector3 upDifferenceRotationAngle = Vector3.Cross(currentCarUp[0], newForward);
+        float upDifferenceAngleSigned = Vector3.SignedAngle(transform.up, currentCarUp[0], upDifferenceRotationAngle);
+
+        Vector3 newForwardRotated = Quaternion.AngleAxis(upDifferenceAngleSigned, upDifferenceRotationAngle) * newForward;
+        transform.rotation = Quaternion.LookRotation(newForwardRotated, currentCarUp[0]);
+
+        Plane plane = new Plane(currentCarUp[0], currentCarUp[1]); // ground plane
+        Ray ray = new Ray(transform.position, -currentCarUp[0]);
+        float distance; // the distance from the ray origin to the ray intersection of the plane
+        if (plane.Raycast(ray, out distance))
+        {
+            transform.position = ray.GetPoint(distance-1f) + transform.forward * track.getSpeed(); // distance along the ray
+            Debug.DrawRay(ray.origin, ray.direction * distance, Color.yellow);
+        } else
+        {
+            Debug.Log("my Ray did not hit :(");
+        }
+    }
+}
