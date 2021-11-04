d8d9921a2b8f10da065425e7e05811a5677286b5
DanielSchwager <danieleric.schwager@stud.hslu.ch>
2021-11-04 14:46:59 +0100

    Camera rotation

--- ControllerTest2/Assets/Scripts/RaceScripts/CameraFollow.cs
+++ ControllerTest2/Assets/Scripts/RaceScripts/CameraFollow.cs
@@ -7,20 +7,28 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
     private int physicUpdateCount = 0;
+    // This value will change at the runtime depending on target movement. Initialize with zero vector.
+    private Vector3 velocity = Vector3.zero;
+
 
     void FixedUpdate()
     {
         if (physicUpdateCount % 2 == 0)
         {
-            transform.rotation = target.transform.rotation;
-            Vector3 desiredPosition = target.position + target.transform.rotation * offset;
-                Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-                transform.position = smoothedPosition;
-                //transform.rotation = Quaternion.LookRotation(target.transform.forward, target.transform.up);
 
+            Vector3 desiredPosition = target.position + target.transform.rotation * offset;
+
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            //Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+
+            transform.position = smoothedPosition;
+
+            transform.LookAt(target);
+            Vector3 targetRotation = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, target.transform.eulerAngles.z);
+            transform.rotation = Quaternion.Euler(targetRotation);
 
         }
         physicUpdateCount++;
-        transform.LookAt(target);
+
     }
 }
